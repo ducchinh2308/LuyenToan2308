94bab28787bc66c8ae8ae30c452ce55f6fa19ca4
ducchinh2308 <64687784+ducchinh2308@users.noreply.github.com>
2026-05-12 18:32:21 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 ﻿// Đặt dòng này ở DÒNG SỐ 1 của file app.js
-const APP_VERSION = "Cập nhật lúc 18h24 - Ngày 12/05";
+const APP_VERSION = "app.js cập nhật lúc 18h24 - Ngày 12/05";
 
 // In ra cửa sổ F12 (Console) với màu nền nổi bật để đập ngay vào mắt
-console.log(`%c🚀 HỆ THỐNG ĐANG CHẠY BẢN: ${APP_VERSION}`, "background: #d35400; color: white; font-size: 14px; padding: 5px; font-weight: bold;");
+console.log(`%c🚀 ĐANG CHẠY KHỐI 1-7 BẢN: ${APP_VERSION}`, "background: #d35400; color: white; font-size: 14px; padding: 5px; font-weight: bold;");
 
 // Nếu thầy lười mở F12, thầy có thể cho nó in luôn một dòng chữ mờ mờ ở góc dưới màn hình:
 window.onload = () => {
