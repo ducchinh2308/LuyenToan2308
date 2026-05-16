5b1b08c89573e65c9bb57875acb30d70847efda2
ducchinh2308 <64687784+ducchinh2308@users.noreply.github.com>
2026-05-16 20:19:56 +0700

    Update khoi10_lambai.js

--- js/khoi10_lambai.js
+++ js/khoi10_lambai.js
@@ -6,7 +6,7 @@ console.log(`%c🚀 ĐANG CHẠY: ${KHOI10_VERSION}`, "background: #28a745; colo
 
 window.addEventListener('load', () => {
     let versionBadge = document.createElement('div');
-    versionBadge.innerHTML = KHOI8_VERSION;
+    versionBadge.innerHTML = KHOI10_VERSION;
     versionBadge.style.cssText = "position: fixed; bottom: 20px; right: 5px; font-size: 9px; color: #28a745; z-index: 9999; ";
     document.body.appendChild(versionBadge);
 });
