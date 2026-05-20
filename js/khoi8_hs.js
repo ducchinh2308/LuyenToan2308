e14b3bd3e055c54f90943c3c93043291bbe8f6e9
ducchinh2308 <64687784+ducchinh2308@users.noreply.github.com>
2026-05-20 17:00:16 +0700

    Update khoi8_hs.js

--- js/khoi8_hs.js
+++ js/khoi8_hs.js
@@ -2150,7 +2150,7 @@ function ham_8_14_ve_giao_dien_xem_lai(ketQua, deThi, nv, baseUrlHinhAnh, choPhe
         let sttPhan = 1;
 
         // Tìm đoạn code duyệt danh sách
-        console.log("DEBUG-LIST:", danh_sach); // Thay danh_sach_cau_hoi_goc bằng tên biến thực tế thầy dùng
+        console.log("DEBUG-LIST:", danhSach); // Thay danh_sach_cau_hoi_goc bằng tên biến thực tế thầy dùng
 
 
         danhSach.forEach(cau => {
